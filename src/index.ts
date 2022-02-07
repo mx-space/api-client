fa03751f287a0399f79d8b9248740b0a5e9c125d
Innei <tukon479@gmail.com>
2022-02-07 21:39:26 +0800

    fix: export missing type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import _camelcaseKeys from 'camelcase-keys'
 
 export * from './controllers'
-export { createClient, HTTPClient, RequestError } from './core'
+export { createClient, RequestError } from './core'
+export type { HTTPClient } from './core'
 export * from './models'
 
 // export helper lib
